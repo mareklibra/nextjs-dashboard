8a34854507a16c4bc34d5da6f888a606a85fe072
Marek Libra <marek.libra@gmail.com>
2024-11-08 14:24:11 +0100

    U to Chapter 11

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // with next@canary only
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 };
 
 export default nextConfig;
